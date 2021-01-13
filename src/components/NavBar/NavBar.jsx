12046ff49a2b9f8201cc3cca07e5b7d7bd3d1c3c
sebastians9517 <sebastians9517@gmail.com>
2021-01-13 13:41:06 -0600

    Erika's page working :D

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = ({ user, handleLogout }) => {
             <ul id="nav-mobile" className="right">
               <li className="nav-link">Welcome, {user.name}</li>
               <li><Link to="/users" className="nav-link">Users</Link></li>
+              <li><Link to="/" className="nav-link">Home</Link></li>
               <Link to='' className='nav-link' onClick={handleLogout}>LOG OUT</Link>
             </ul>
           </div>
@@ -18,6 +19,7 @@ const NavBar = ({ user, handleLogout }) => {
         <nav>
           <div className="nav-wrapper">
             <ul id="nav-mobile" className="right">
+            <li><Link to="/" className="nav-link">Home</Link></li>
               <li><Link to="/login" className="nav-link">Log In</Link></li>
               <li><Link to="/signup" className="nav-link">Sign Up</Link></li>
             </ul>
